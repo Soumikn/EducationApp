b8ea4e4c55d7d601f6d2e13d02e92fbad6ecf288
Amine <vipamine@gmailcom>
2017-12-12 18:30:34 +0100

    Emploi.class

--- app/src/main/java/com/example/asus/educationapp/EmploiFragment.java
+++ app/src/main/java/com/example/asus/educationapp/EmploiFragment.java
@@ -1,18 +1,30 @@
 package com.example.asus.educationapp;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EmploiFragment extends Fragment {
-
+    public int ccc;
+    SharedPreferences sp;
+    Uri selectedImage;
+    boolean empExist; // Emploi mawjoud walla lee
 
     public EmploiFragment() {
         // Required empty public constructor
@@ -23,7 +35,54 @@ public class EmploiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        sp = getActivity().getSharedPreferences("emp",Context.MODE_PRIVATE);
+
+        empExist = sp.getBoolean("empExist",false);
+
+        if(!empExist){
+            openGallery();
+
+
+        }
         return inflater.inflate(R.layout.fragment_emploi, container, false);
+    }
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select File"),1);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //CropImage.ActivityResult result = CropImage.getActivityResult(data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+
+                selectedImage = data.getData();
+                ccc = 1;
+                Log.d("IMAGE","FIRST !!");
+                CropImage.activity(selectedImage).start(getContext(),this);
+
+            }
+
+            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                Log.d("IMAGE","SECOND !!");
+                Uri resultUri = result.getUri();
+                sp.edit().putString(getString(ccc),resultUri.toString()).apply();
+                ccc++;
+                if(ccc < 8){
+                    CropImage.activity(selectedImage).start(getContext(),this);
+                    ccc++;
+                }
+            }
+
+        }else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            Exception error = result.getError();
+            Log.d("ERREUR",error.toString());
+        }
     }
 
 }
